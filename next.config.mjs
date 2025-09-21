0d0a9537ba3fd2269bc0f6291238e1c6eb8784e5
Teleringer <teleringerllc@gmail.com>
2025-09-21 15:37:58 -0400

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  // If images are only local, leave images block out.
+  // If you later load external images, we can add `images.remotePatterns`.
+};
+
+export default nextConfig;
